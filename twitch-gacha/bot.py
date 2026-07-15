5247219deab329a0027f009e32b1a87d0d4ecf29
DUNJUNPC\dunJun <francisco.p.pimenta@outlook.com>
2026-07-15 17:20:54 +0100

    cors updated

--- twitch-gacha/bot.py
+++ twitch-gacha/bot.py
@@ -9,10 +9,11 @@ from twitchio import eventsub
 from flask import Flask
 from threading import Thread
 from flask import jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/",methods=["GET"])
 def home():
